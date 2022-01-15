42306241677c16bcaf7b12e5237513e2a7ade165
Watthachai Taechalue <wattchaichai@gmail.com>
2022-01-15 19:36:31 +0700

    addphone login

---
+++ login-phone-test/js/profile.js
@@ -0,0 +1,32 @@
+const logOut = document.getElementById('logOut');
+const mergeAccounts = document.getElementById('mergeAccounts');
+const modifyAccount = document.getElementById('modifyAccount');
+const displayNameHolder = document.getElementById('displayNameHolder');
+const photoHolder = document.getElementById('photoHolder');
+
+const auth = firebase.auth();
+
+logOut.addEventListener('click', () => {
+    //signOut() is a built in firebase function responsible for signing a user out
+    auth.signOut()
+    .then(() => {
+        window.location.assign('../');
+    })
+    .catch(error => {
+        console.error(error);
+    })
+})
+
+auth.onAuthStateChanged(user => {
+    console.log(user);
+})
+
+//Go to modification page
+modifyAccount.addEventListener('click', () => {
+    window.location.assign('../edit');
+});
+
+//Go to merge accounts page
+mergeAccounts.addEventListener('click', () => {
+    window.location.assign('../merge');
+});
